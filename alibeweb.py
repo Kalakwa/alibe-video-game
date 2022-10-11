8385e6a2fd8b81de36d6dc8b62fbb30aeed9067c
Daniel <ddickerson180@gmail.com>
2022-10-11 21:59:39 +0200

    initial commit

---
+++ alibeweb.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='Alibe', page_icon=':tada:', layout='wide')
+
+#header section
+with st.container():
+    st.subheader('The first video game in the series!')
+    st.title('Alibe: Libb Like Me')
+    st.write('Hello! I am very glad you are able to join in on this journey of being '
+             'alibe with me! I am Shaman ZONGAZ, a profound Shaman who shifts this '
+             'realm through artistic depiction. This artistic project, ALIBE: '
+             'Libb Like Me, is a project I started as a thank you to my dear '
+             'partner Elizabeth, for all of the support and lessons she has '
+             'lended me through life. She has truly taught me to view the world '
+             'with a peculiar lens and live every day to the fullest.'
+             'Follow my work here: ')
+
+#What it does
+with st.container():
+    st.header('What\'s Alibe About?')
+    st.write('##')
+    st.write('In this mystic revolving world, we will portray a fluid and intangible '
+             'sense of ideas to manifest a sense of upliftment and encouragement '
+             'within each player\'s identity of themselves. We do this with the '
+             'gameplay, music, artwork and overall feel of the game as the player '
+             'progresses through the storyline and then finds that ultimate sanctuary '
+             'in a liberated world! Below is a contents list that will enable you to '
+             'navigate through the documents among the folder. I wish you the best, '
+             'and please reach out to my email with any questions regarding the '
+             'project. I hope you are ready to make a powerful impact in the world we Libb.'
+             ''
+             'If this sounds interesting to you, follow along my pages in my media page and '
+             'turn on notifications!')
---
+++ alibeweb.py.py
@@ -0,0 +1,62 @@
+from PIL import Image
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+
+st.set_page_config(page_title='Alibe', page_icon=':unamused:', layout='wide')
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Load assets
+lottie_coding = load_lottieurl('https://assets9.lottiefiles.com/packages/lf20_y2nlvxyz.json')
+lottie_manny = Image.open('Pictures/manny.gif')
+#header section
+with st.container():
+    st.title('Alibe')
+    st.subheader('Where dreams meet reality...')
+    st.write('On a Planet formed from combining the Lands of Physical, Spiritual, Space, Time, Light, '
+             'and Sound, to become the 6 realms of consciousness, you decide the fate of the Planet Alibe... ')
+
+#Features of Alibe
+with st.container():
+    st.write('-------------')
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header('Includes')
+        st.write('- A fluid and intangible sense of ideas to manifest a sense of upliftment and '
+                 'encouragement within each player through a world of magic and adventure gameplay.')
+        st.write('- Explore 6 different realms and mysterious caves within Alibe, magic at hand')
+        st.write('- Create the ultimate sanctuary in a liberated world! Use a variety of tools to '
+                 'restore the land with different vegetation, including ingredients for Alchemy and cooking! ')
+        st.write('- Find the right way to express yourself in Alibe with over 500 customization options'
+                 'for your character!')
+        st.write('- Journey into the mastery of 11 different classes to choose from, including the Sigil of '
+                 'the Knights, Gardners, Geocachers, Mystics and more!')
+
+
+        st.write('I wish you the best, and please reach out to my email \(found at the bottom\) '
+                 'with any questions regarding the project. I have placed a request for information '
+                 'at the bottom where you may enter your name, email, and add some notes are comments'
+                 'you wish to share directly with me, the dev. I hope you are ready to make a powerful '
+                 'impact in the world we Libb. Follow along my social media pages and turn on notifications '
+                 'to get ready for Alibe: Libb Like Me!')
+    with right_column:
+        st_lottie(lottie_coding, height=300, key='coding')
+
+
+with st.container():
+    st.write('~~~~~~~~~')
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(lottie_manny)
+
+
+
+
+
+#art stuff
