b709160d71e4ecaccc73a90f71fac258e54cb989
Daniel <ddickerson180@gmail.com>
2022-10-18 00:18:55 +0200

    Form Submission Update

--- alibeweb.py.py
+++ alibeweb.py.py
@@ -13,6 +13,11 @@ def load_lottieurl(url):
         return None
     return r.json()
 
+def local_css(file_name):
+    with open(file_name) as fi:
+        st.markdown(f"<style>{fi.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
 
 # Load assets
 lottie_coding = load_lottieurl(
@@ -58,5 +63,20 @@ with st.container():
     with image_column:
         st.image(lottie_manny)
 
+
+contact_form = """
+<form action="https://formsubmit.co/mortapolis@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Your name" required>
+    <input type="email" name="email" placeholder="Your email" required>
+    <textarea name="message" placeholder="Your message here" required></textarea>
+    <button type="submit">Send</button>
+</form>
+"""
+left_column, right_column = st.columns(2)
+with left_column:
+    st.markdown(contact_form, unsafe_allow_html=True)
+with right_column:
+    st.empty()
 
 # art stuff
